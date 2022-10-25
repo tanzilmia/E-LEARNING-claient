d251820d1e50f1a9605aafc98e05d1b036bc1a56
tanzilmia <ziltan590@gmail.com>
2022-10-25 20:56:09 +0600

    add npm pakeges

---
+++ src/component/CoursePage/CoursePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoursePage = () => {
+    return (
+        <div>
+            <h2>CoursePage</h2>
+        </div>
+    );
+};
+
+export default CoursePage;
---
+++ src/component/courseContent/CourseContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CourseContent = () => {
+    return (
+        <div>
+            <h2>Course Context</h2>
+        </div>
+    );
+};
+
+export default CourseContent;
