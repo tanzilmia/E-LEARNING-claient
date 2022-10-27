5572d639a89e67115fd4a2a6da53d95122fb89a0
tanzilmia <ziltan590@gmail.com>
2022-10-27 17:18:04 +0600

    readme file add

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../component/Footer/Footer';
 import Navbars from '../component/Navbar/Navbar';
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
         <div>
             <Navbars></Navbars>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className='bg-info p-3 mt-4 text-center text-light'>
+            <h4>Copyright by Tanzil | 2022 </h4>
+        </div>
+    );
+};
+
+export default Footer;
